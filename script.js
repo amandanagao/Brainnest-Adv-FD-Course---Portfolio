f06c36b7ef728a0d0450b55bda7225da6ba4cbe6
davidIvankov <david.ivankov96@gmail.com>
2023-04-13 17:19:17 +0200

    feat started working on my work section

--- script.js
+++ script.js
@@ -3,12 +3,33 @@ navLinks = document.querySelectorAll('.nav-link'),
 imig = document.getElementById('imig'),
 figcap = document.getElementById('figcap'),
 site = document.getElementById('site'),
-initAnimation = document.getElementById('init-animation')
+initAnimation = document.getElementById('init-animation'),
+hr = document.getElementById('hr'),
+h1 = document.getElementById('h1');
+
+const animation = function(){
+  let textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: (el, i) => 45 * i
+  });
+}
+
+hr.addEventListener('animationend',()=>{
+  h1.classList.remove('hidden')
+  animation()
+})
+
 
 navLinks.forEach(navLink =>{
     navLink.addEventListener('click',()=>{
-        navLinks.forEach(navLink => navLink.classList.remove('link-active'))
-        navLink.classList.add('link-active')
+        navLinks.forEach(navLink => navLink.parentElement.classList.remove('link-active'))
+        navLink.parentElement.classList.add('link-active')
     })
 })
 imig.addEventListener('animationend', ()=>{
